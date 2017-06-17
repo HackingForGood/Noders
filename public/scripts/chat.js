564004ab193aeb0611ed9481d575a95e293eb5d8
Ankit Kumar <ankitkumarr@users.noreply.github.com>
2017-06-17 15:19:54 -0400

    Updated chat files

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -16,17 +16,16 @@
 'use strict';
 
 // Initializes Chat.
-function Chat() {
-    this.checkSetup();
+function Chat(user, chatId) {
 
+  //TODO: Check if user is authenticated
     // Shortcuts to DOM Elements.
+    this.user = user;
+    this.chatId = chatId;
     this.messageList = document.getElementById('messages');
     this.messageForm = document.getElementById('message-form');
     this.messageInput = document.getElementById('message');
     this.submitButton = document.getElementById('submit');
-    this.submitImageButton = document.getElementById('submitImage');
-    this.imageForm = document.getElementById('image-form');
-    this.mediaCapture = document.getElementById('mediaCapture');
     this.userPic = document.getElementById('user-pic');
     this.userName = document.getElementById('user-name');
 
@@ -43,126 +42,28 @@ function Chat() {
         e.preventDefault();
         this.mediaCapture.click();
     }.bind(this));
-    // this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
+    window.setInterval(this.loadMessages, 1000);
 }
 
 // Loads Chat messages history and listens for upcoming ones.
 Chat.prototype.loadMessages = function() {
-  // Reference to the /messages/ database path.
-    // TODO: Get messages
-  this.messagesRef = this.database.ref('messages');
-  // Make sure we remove all previous listeners.
-  this.messagesRef.off();
-  // Loads the last 12 messages and listen for new ones.
-  var setMessage = function(data) {
-    var val = data.val();
-    this.displayMessage(data.key, val.name, val.text, val.photoUrl, val.imageUrl);
-  }.bind(this);
-  this.messagesRef.limitToLast(12).on('child_added', setMessage);
-  this.messagesRef.limitToLast(12).on('child_changed', setMessage);
+
+    Firebase.chat.getChatMessages(this.chatId, function (messages) {
+      messages.forEach(function (val) {
+          this.displayMessage(val.key,val.name, val.text, val.profilepic)
+      });
+    });
 };
 
 // Saves a new message on the Firebase DB.
 Chat.prototype.saveMessage = function(e) {
-  e.preventDefault();
-  // Check that the user entered a message and is signed in.
-  if (this.messageInput.value && this.checkSignedInWithMessage()) {
-    var currentUser = this.auth.currentUser;
-    // Add a new message entry to the Firebase Database.
-    this.messagesRef.push({
-      name: currentUser.displayName,
-      text: this.messageInput.value,
-      photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
-    }).then(function() {
-      // Clear message text field and SEND button state.
-      Chat.resetMaterialTextfield(this.messageInput);
-      this.toggleButton();
-    }.bind(this)).catch(function(error) {
-      console.error('Error writing new message to Firebase Database', error);
+    Firebase.chat.sendMessage(this.messageInput.value, this.chatId, function () {
+        this.loadMessages();
+        Chat.resetMaterialTextfield(this.messageInput);
+        this.toggleButton();
     });
-  }
 };
 
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-Chat.prototype.onAuthStateChanged = function(user) {
-  if (user) { // User is signed in!
-    // Get profile pic and user's name from the Firebase user object.
-    var profilePicUrl = user.photoURL;
-    var userName = user.displayName;
-
-    // Set the user's profile pic and name.
-    this.userPic.style.backgroundImage = 'url(' + (profilePicUrl || '/images/profile_placeholder.png') + ')';
-    this.userName.textContent = userName;
-
-    // Show user's profile and sign-out button.
-    this.userName.removeAttribute('hidden');
-    this.userPic.removeAttribute('hidden');
-    this.signOutButton.removeAttribute('hidden');
-
-    // Hide sign-in button.
-    this.signInButton.setAttribute('hidden', 'true');
-
-    // We load currently existing chant messages.
-    this.loadMessages();
-
-    // We save the Firebase Messaging Device token and enable notifications.
-    this.saveMessagingDeviceToken();
-  } else { // User is signed out!
-    // Hide user's profile and sign-out button.
-    this.userName.setAttribute('hidden', 'true');
-    this.userPic.setAttribute('hidden', 'true');
-    this.signOutButton.setAttribute('hidden', 'true');
-
-    // Show sign-in button.
-    this.signInButton.removeAttribute('hidden');
-  }
-};
-
-// Returns true if user is signed-in. Otherwise false and displays a message.
-Chat.prototype.checkSignedInWithMessage = function() {
-  // Return true if the user is signed in Firebase
-  if (this.auth.currentUser) {
-    return true;
-  }
-
-  // Display a message to the user using a Toast.
-  var data = {
-    message: 'You must sign-in first',
-    timeout: 2000
-  };
-  this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
-  return false;
-};
-
-// Saves the messaging device token to the datastore.
-Chat.prototype.saveMessagingDeviceToken = function() {
-
-  // TODO: Need to update once backend is complete
-  firebase.messaging().getToken().then(function(currentToken) {
-    if (currentToken) {
-      console.log('Got FCM device token:', currentToken);
-      // Saving the Device Token to the datastore.
-      firebase.database().ref('/fcmTokens').child(currentToken)
-          .set(firebase.auth().currentUser.uid);
-    } else {
-      // Need to request permissions to show notifications.
-      this.requestNotificationsPermissions();
-    }
-  }.bind(this)).catch(function(error){
-    console.error('Unable to get messaging token.', error);
-  });
-};
-
-// Requests permissions to show notifications.
-Chat.prototype.requestNotificationsPermissions = function() {
-  console.log('Requesting notifications permission...');
-  firebase.messaging().requestPermission().then(function() {
-    // Notification permission granted.
-    this.saveMessagingDeviceToken();
-  }.bind(this)).catch(function(error) {
-    console.error('Unable to get permission to notify.', error);
-  });
-};
 
 // Resets the given MaterialTextField.
 Chat.resetMaterialTextfield = function(element) {
@@ -182,7 +83,7 @@ Chat.MESSAGE_TEMPLATE =
 Chat.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
 
 // Displays a Message in the UI.
-Chat.prototype.displayMessage = function(key, name, text, picUrl, imageUri) {
+Chat.prototype.displayMessage = function(key, name, text, picUrl) {
   var div = document.getElementById(key);
   // If an element for that message does not exists yet we create it.
   if (!div) {
@@ -218,6 +119,8 @@ Chat.prototype.toggleButton = function() {
   }
 };
 
-window.onload = function() {
-  window.chat = new Chat();
+haven.firebaseScriptInjectLoaded = function() {
+  Firebase.getActiveChatID(Firebase.getCurrentUser().uid, function(chatId) {
+    window.chat = new Chat(Firebase.getCurrentUser(), chatID);
+  });
 };
